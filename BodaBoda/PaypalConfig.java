41fb2de7720011b92539344639c8c95f4a7fd94a
Jonah Karani <jonahkarash@gmail.com>
2019-06-25 08:00:11 +0300

    Add files via upload

---
+++ BodaBoda/PaypalConfig.java
@@ -0,0 +1,12 @@
+package com.simcoder.uber;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PaypalConfig{
+    public static final String PAYPAL_CLIENT_ID = "";
+}
